08c37ca54f63063b7402d12e9bab45d90ac13c03
LucioFider <fidelislucio404@gmail.com>
2023-03-27 14:19:42 +1000

    3rd-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Challenge01 from "./challenges/Challenge01";
 import DescribingTheUi from "./components/DescribingTheUi";
+import Gallery from "./components/Profile";
 
 function App() {
   return (
     <div>
       <h1>Describing the UI</h1>
-      <DescribingTheUi />
-      <Challenge01 />
+      {/* <DescribingTheUi />
+      <Challenge01 /> */}
+      <Gallery />
     </div>
   );
 }
